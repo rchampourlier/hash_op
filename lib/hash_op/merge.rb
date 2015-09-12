c99b97e2769c7a6cea7b071212da2c18fca1d1ba
Romain Champourlier <pro@rchampourlier.com>
2015-09-13 01:23:51 +0200

    Renaming in Merge
    - Merge.merge => Merge.flat
    - Merge.merge_by_group => Merge.by_group

--- lib/hash_op/merge.rb
+++ lib/hash_op/merge.rb
@@ -3,19 +3,19 @@ module HashOp
 
     # Merge all specified hashes by merging the second
     # in the first, the third in the result, and so on.
-    def merge(hashes)
+    def flat(hashes)
       hashes.inject({}) do |result, hash|
         result.merge hash
       end
     end
-    module_function :merge
+    module_function :flat
 
     # Merge hashes by grouping them on the
     # specified key value and merging them all together.
-    def merge_by_group(hashes, key)
+    def by_group(hashes, key)
       groups = hashes.group_by { |h| h[key] }
-      groups.values.map { |g| merge(g) }
+      groups.values.map { |g| flat(g) }
     end
-    module_function :merge_by_group
+    module_function :by_group
   end
 end
--- spec/hash_op/merge_spec.rb
+++ spec/hash_op/merge_spec.rb
@@ -3,38 +3,40 @@ require 'hash_op/merge'
 
 describe HashOp::Merge do
 
-  describe '::merge(hashes)' do
-
-    it 'should return the resulting hash of merging all specified hashes' do
-      hashes = [
+  describe '::flat(hashes)' do
+    subject { described_class.flat(hashes) }
+    let(:hashes) do
+      [
         { a: 1, b: 2},
         { c: 3, d: 4},
         { a: 2, e: 5}
       ]
-      result = described_class.merge(hashes)
-      expect(result).to eq({
+    end
+
+    it 'returns the resulting hash of merging all specified hashes' do
+      expect(subject).to eq(
         a: 2,
         b: 2,
         c: 3,
         d: 4,
         e: 5
-      })
+      )
     end
   end
 
-  describe '::merge_by_group(array, key)' do
+  describe '::by_group(array, key)' do
 
     it 'should merge hashes grouped on the specified key' do
       array = [
-        { a: 1, b: 2},
-        { a: 2, b: 1},
-        { a: 1, c: 3},
-        { a: 2, b: 2, c: 2}
+        { a: 1, b: 2 },
+        { a: 2, b: 1 },
+        { a: 1, c: 3 },
+        { a: 2, b: 2, c: 2 }
       ]
-      result = described_class.merge_by_group(array, :a)
+      result = described_class.by_group(array, :a)
       expect(result).to eq([
-        { a: 1, b: 2, c: 3},
-        { a: 2, b: 2, c:2 }
+        { a: 1, b: 2, c: 3 },
+        { a: 2, b: 2, c: 2 }
       ])
     end
   end
